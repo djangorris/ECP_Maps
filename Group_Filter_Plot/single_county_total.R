96501da8fcc2a4887b925230b988ea8202c48fbb
djangorris <lukkyjay@gmail.com>
2018-04-27 14:46:56 -0600

    Copied improvements from the ProviderMaps repo

--- Group_Filter_Plot/single_county_total.R
+++ Group_Filter_Plot/single_county_total.R
@@ -1,17 +1,25 @@
 ##### SINGLE COUNTY TOTAL  #####
-county_selected <- "Summit"
-SINGLE_COUNTY <- ALL_county_specialty_count %>% 
-  filter(County == county_selected) %>% 
+selected_county <- "El Paso"
+clean_selected_county <- str_replace_all(selected_county, " ", "_")
+SINGLE_COUNTY <- ALL_county_specialty_count %>%
+  filter(County == county_selected) %>%
   summarise(Total = sum(nproviders, na.rm = TRUE))
-cols <- c("Anthem BCBS" = "blue", 
-          "Bright" = "deeppink3", 
+cols <- c("Anthem BCBS" = "blue",
+          "Bright" = "deeppink3",
           "Cigna" = "forestgreen",
           "Denver Health" = "navyblue",
           "Kaiser" = "deepskyblue2",
           "RMHP" = "darkorange")
 ggplot(SINGLE_COUNTY, aes(x = Carrier, y = Total, fill = Carrier)) +
   geom_bar(position="dodge", stat="identity") +
-  xlab("Insurance Carrier") +
-  ylab("Number of Providers") +
-  ggtitle(str_c(county_selected, " County Medical Providers")) +
-  scale_fill_manual(values = cols)
+  xlab(" ") +
+  ylab(NULL) +
+  ggtitle(str_c(selected_county, " County Medical Providers")) +
+  scale_fill_manual(values = cols) +
+  labs(caption = "  Graphic by Colorado Health Insurance Insider / @lukkyjay                                                                                                                                                           Source: SERFF") +
+  theme(plot.margin = margin(5, 5, 5, 5),
+        plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=18, hjust=0),
+        legend.position = "none",
+        plot.caption = element_text(family = "Arial", size = 10, color = "grey", hjust = 0.5)) +
+  ggsave(filename = paste0(here("/"), "Plots/", clean_selected_county, "_total.png"),
+         width = 12, height = 8, dpi = 1200)
--- Group_Filter_Plot/statewide_enrollment_2017.R
+++ Group_Filter_Plot/statewide_enrollment_2017.R
@@ -1,25 +1,27 @@
-###### STATEWIDE BY SPECIALTY #####
-selected_specialty <- "003 Internal Medicine"
-clean_selected_specialty <- str_replace_all(selected_specialty, " ", "_")
-STATEWIDE <- ALL_statewide_specialty_count %>%
-  filter(Specialty == selected_specialty)
-# plot
+################################################
+## ENROLLMENT DATA
+################################################
+members <- c(55860, 16666, 21728, 675, 110000, 3611)
+carrier <- c("Anthem BCBS", "Bright", "Cigna", "Denver Health", "Kaiser", "RMHP")
+ind_enrollment_2017 <- data.frame("Carrier" = carrier, "Members" = members)
+# Plot
 cols <- c("Anthem BCBS" = "blue",
           "Bright" = "deeppink3",
           "Cigna" = "forestgreen",
           "Denver Health" = "navyblue",
           "Kaiser" = "deepskyblue2",
           "RMHP" = "darkorange")
-ggplot(STATEWIDE, aes(x = Carrier, y = count, fill = Carrier)) +
+ggplot(ind_enrollment_2017, aes(x = Carrier, y = Members, fill = Carrier)) +
   geom_bar(position="dodge", stat="identity") +
+  geom_text(aes(label= format(Members, big.mark=",", trim=TRUE)), vjust = -0.5) +
   xlab(" ") +
   ylab(NULL) +
-  ggtitle(str_c("Providers listed as ", '"', selected_specialty, '" in Colorado')) +
+  ggtitle("Colorado Statewide Medical Membership Enrollment") +
   scale_fill_manual(values = cols) +
   labs(caption = "  Graphic by Colorado Health Insurance Insider / @lukkyjay                                                                                                                                                           Source: SERFF") +
   theme(plot.margin = margin(5, 5, 5, 5),
         plot.title = element_text(family = "Trebuchet MS", color="#666666", face="bold", size=18, hjust=0),
         legend.position = "none",
         plot.caption = element_text(family = "Arial", size = 10, color = "grey", hjust = 0.5)) +
-  ggsave(filename = paste0(here("/"), "Plots/", "statewide_", clean_selected_specialty, ".png"),
+  ggsave(filename = paste0(here("/"), "Plots/statewide_enrollment.png"),
          width = 12, height = 8, dpi = 1200)
